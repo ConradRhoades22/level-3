0f13cb51dae636fb9dcaf3d64be9c17984ba1494
Conrad Rhoades <conradrhoades@gmail.com>
2023-01-27 11:22:06 -0800

    fixed linkedin button

--- level-3/projects/digital-buisness-card/src/components/links.js
+++ level-3/projects/digital-buisness-card/src/components/links.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Links() {
     return (
     <div>
-        <a>
-            <button href="https://www.linkedin.com/in/conrad-rhoades-259959160/" className="Linkedin">Linkedin</button>
+        <a href="https://www.linkedin.com/in/conrad-rhoades-259959160/">
+            <button className="Linkedin">Linkedin</button>
         </a>
         <a>
             <button onClick={() => navigator.clipboard.writeText('conradrhoades@gmail.com')} className="Email">
